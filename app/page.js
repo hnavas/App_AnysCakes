893df6e9303ffaa09b42b418f6021d12c14fd659
hnavas <hendri.navas@gmail.com>
2023-06-18 08:11:51 -0300

    NectAuthjs

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
-import Image from 'next/image'
+'use client'
+
+import GoogleButton from "react-google-button"
+import { signIn } from "next-auth/react"
 
 export default function Home() {
-  return (
-    <div className={'bg-orange-400'}>Hello Anys Cakes</div>
-  )
+    return (
+      <main className="bg-slate-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <GoogleButton onClick={() => signIn('google')} className="mx-auto mt-16"/>
+        </div>
+      </main>
+    );
 }
